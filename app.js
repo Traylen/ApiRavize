554f1fc3e6f5decbdc38411a506c4d9c6a0c6bfc
Stefan Dupont <dupontstef80@gmail.com>
2025-05-07 18:42:20 +0200

    refactor(structure): moved users routes in other file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const db = require('./db');
 const jwt = require('jsonwebtoken')
 const swaggerUi = require('swagger-ui-express')
 const swaggerJsdoc = require('swagger-jsdoc');
+global.app = express();
+const {middleWare, router: userRoute} = require('./controller/users');
 
 const options = {
     definition: {
@@ -22,128 +24,25 @@ const options = {
       }
     },
   },
-  apis: ['./app.js'], // files containing annotations as above
+  apis: ['./app.js', './controller/*.js'], // files containing annotations as above
 }
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)))
-
-
-/**
- * @swagger
- * /user/register:
- *   post:
- *     description: adding a new user
- *     tags:
- *       - user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *     responses:
- *       200:
- *         description: user registered
- */
-app.post("/user/register", async(req, res) => {
-    try {
-        const username = req.body.username;
-        const password = req.body.password;
-        
-        await db.query("INSERT INTO user(name, hashed_password) value (?, ?)", [username, password])
-        res.status(200).json({ message: "User added succefuly"})
-    } catch (err) {
-        res.status(500).json({ erreur: err.message})
-    }
-}) 
+app.use("/user", userRoute)
 
 /**
  * @swagger
- * /user/login:
+ * /list/create:
  *   post:
- *     tags:
- *       - user
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               username:
- *                 type: string
- *               password:
- *                 type: string
- *     description: Welcome to swagger-jsdoc!
- *     responses:
- *       200:
- *         description: Returns a mysterious string.
- */
-app.post("/user/login", async(req, res) => {
-    try {
-        const username = req.body.username;
-        const password = req.body.password;
-
-        console.log(username)
-        console.log(password);
-
-        const [response] = await db.query("SELECT * from user where name = ? and hashed_password = ?", [username, password])
-
-        if(response.length <= 0) return res.status(401).json({error: "user not find"})
- 
-        console.log(response[0].name)
-        
-        const token = jwt.sign({ userId: response[0].id, name: response[0].name }, 'your-secret-key', {expiresIn: '1800s'});
-
-        res.json({token})
-    } catch (err) {
-        res.status(500).json({ erreur: err.message})
-    }
-})
-
-function middleWare(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-
-      if (token == null) return res.sendStatus(401)
-
-      jwt.verify(token, "your-secret-key", (err, user) => {
-        console.log(user)
-
-        if (err) return res.sendStatus(403)
-
-        req.user = user
-
-        next()
-    })
-}
-
-/**
- * @openapi
- * /user/profile:
- *   get:
- *     tags:
- *       - user
  *     security:
  *       - bearerAuth: []
- *     description: Welcome to swagger-jsdoc!
- *     responses:
- *       200:
- *         description: Returns a mysterious string.
+ *     description: creating a new list
  */
-app.get("/user/profile", middleWare ,async(req, res) => {
-    // res.status(200).json({message: "e " + req.headers['authorization']})
-    res.status(200).json({message: `connecté`, user: req.user})
-    // res.status(200).json({message: jwt.verify(req.header("Authorization"), "your-secret-key")})
+app.post("/list/create", middleWare, async(req,res) => {
+
 })
 
 /**
@@ -213,6 +112,7 @@ app.get('/api/produits/categorie/:categorie', async (req, res) => {
         res.status(500).json({ erreur: err.message });
     }
 });
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`API REST en écoute sur http://localhost:${PORT}`);
