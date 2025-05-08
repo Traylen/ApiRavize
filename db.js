4942ca89f7142ed999e16247432e3ca6ba842c1f
unknown <dupontstef80@gmail.com>
2025-05-08 17:33:07 +0200

    fix(ORM): missing env relation

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
-global.sequelize = new Sequelize('ravize', 'root', 'root', {
-    host: '127.0.0.1',
+global.sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: "mariadb"
 })
 
